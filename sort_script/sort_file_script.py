9b389ba8353fe4ae42a48fe4e4d66594633c38f4
EBSGTG <gerty438@gmail.com>
2024-09-27 17:45:49 +0300

    Base version ( P.S. ChatGPT rated 7/10 :) )

--- sort_script/sort_file_script.py
+++ sort_script/sort_file_script.py
@@ -1,8 +1,9 @@
-
+from operator import itemgetter
 
 def main():
     first_data = read_file("sort_file_script.txt")
     handled_data, last = handle_data(first_data,2)
+    handled_data = sort_numbers(handled_data)
     finder(handled_data)
     write_file("sort_file_script_new.txt", handled_data, last)
 
@@ -30,29 +31,66 @@ def handle_data(data, n):
         print(e)
     return list_of_dicts, names_of_parameters[-1]
 
+
+def sort_by_letters(data):
+    try:
+        def casting():
+            s = input("Enter a column to sort: ")
+            print("1 - Ascending")
+            print("2 - Descending")
+            print("3 - Exit")
+            b = True
+            if s == 1:
+                b = False
+            elif s == 2:
+                b = True
+            elif s == 3:
+                return
+            else:
+                casting()
+            return s, b
+        column, reversing = casting()
+        list_for_bubble = sorted(data, key=itemgetter(column),reverse=reversing)
+        return list_for_bubble
+    except Exception as e:
+        print(e)
+
+
+
+def sort_numbers(data):
+    try:
+        s = input("Enter a column name: ")
+        list_for_bubble = sorted(data, key=itemgetter(s), reverse=True)
+        return list_for_bubble
+    except Exception as e:
+        print(e)
+
+
+
 def finder(data):
     print("Input what you want to find: ")
     input_data = input()
+    found = False
     for line in data:
         for key, value in line.items():
             if input_data in str(value):
                 print(f"Found in line: {data.index(line) + 1}")
+                found = True
+    if not found:
+        print(f"No {input_data} found")
 
 
 def write_file(file_name,data, last):
     try:
         with open(file_name, "w", encoding="utf-8") as file:
             for line in data:
-
                 for key, value in line.items():
                     file.write(value + ", ") if key != last else file.write(value)
                 file.write("\n")
-
+        file.close()
+        print("File saved")
     except IOError:
         print("File not found: " + file_name)
-
-
-
 
 
 
